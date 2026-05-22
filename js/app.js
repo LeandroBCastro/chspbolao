fbb95465e7d60f064adc0f1582f9e76477b0dd92
LeandroBCastro <leandro_bcastro@hotmail.com>
2026-05-22 16:14:44 -0300

    app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 console.log("CHSP Bolão");
+
+async function carregarRanking() {
+  const resposta = await fetch("data/ranking.json");
+  const dados = await resposta.json();
+
+  // ordena do maior para o menor
+  dados.sort((a, b) => b.pontos - a.pontos);
+
+  const tbody = document.getElementById("tabela-ranking");
+  tbody.innerHTML = "";
+
+  dados.forEach((item, index) => {
+    const linha = document.createElement("tr");
+
+    linha.innerHTML = `
+      <td>${index + 1}</td>
+      <td>${item.nome}</td>
+      <td>${item.pontos}</td>
+    `;
+
+    tbody.appendChild(linha);
+  });
+}
+
+carregarRanking();
